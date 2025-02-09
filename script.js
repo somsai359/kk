a0082a7a8c3d1a92b1846fc8cc455673c04afac3
SOMASAI DARRURU <76832603+somsai359@users.noreply.github.com>
2025-02-09 13:17:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Chanduu'; // Replace with your secret code
+const secretCode = 'somsai'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
